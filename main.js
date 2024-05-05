bcc1d1d03890132e4052471c2a6b9cc5e9c0e08e
NRG55 <maloj_1980@yahoo.fr>
2024-05-05 04:32:44 +0100

    Created modules project.js and todo-list.js. Each module is a class.

--- main.js
+++ main.js
@@ -1,1 +1,1 @@
-console.log("Hey");
+(()=>{"use strict";class s{constructor(s){this.name=s,this.tasks=[]}setTask(s){this.tasks=s}}console.log("Hey");let t=new class{constructor(){this.projects=[],this.projects.push(new s("Test Project"))}};console.log(t);let e=new s("Test Project 2");e.setTask("Do the test"),console.log(e)})();
